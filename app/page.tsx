458a85bf8026171cd7c490f4259594227153bccd
Yousuf Elkhoga <yousuf.elkhoga@gmail.com>
2024-05-03 11:06:58 -0400

    need to fix multiple toggle bug

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function IndexPage() {
         </div>
       </section>
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-        <div className="flex max-w-[980px] justify-between gap-2">
+        <div className="flex  max-w-[980px] flex-col justify-between gap-2 sm:flex-row">
           <ToggleGroup type="multiple">
             <ToggleGroupItem variant="outline" value="funeral">
               Janaza
